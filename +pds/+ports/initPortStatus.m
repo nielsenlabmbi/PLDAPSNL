8ce97b4ec41018ba719f83954cd2e3e1408bb5e9
nielsenlabmbi <nielsenlabmbi@gmail.com>
2017-05-16 16:30:07 -0400

    camera start

--- +pds/+ports/initPortStatus.m
+++ +pds/+ports/initPortStatus.m
@@ -3,7 +3,7 @@ function p = initPortStatus(p)
 %initialize a vector with the status and position of each port
 %11/25/16 - kn wrote it
 
-if(p.trial.ports.use)
+if p.trial.ports.use
    p.trial.ports.status=zeros(1,p.trial.ports.nPorts);
    
    if p.trial.ports.movable
--- SupportFunctions/Utils/makePhotodiodeRect.m
+++ SupportFunctions/Utils/makePhotodiodeRect.m
@@ -1,8 +1,8 @@
-function dv = makePhotodiodeRect(dv)
-% rect = makePhotodiodeRect(dv, location)
+function p = makePhotodiodeRect(p)
+% rect = makePhotodiodeRect(p)
 % makePhotodiodeRect makes a 4 coordinate rectangle for PTB fillRect calls
 % INPUTS
-%   dv [struct]
+%   p [struct]
 %     .trial
 %       .pldaps             %pldaps internal parameters
 %         .draw
@@ -23,22 +23,22 @@ function dv = makePhotodiodeRect(dv)
 % 12/15/2013 jly    Wrote it
 % 06/03/2014 jk     made compatible with pldaps class
 
-if(dv.defaultParameters.pldaps.draw.photodiode.use)
-    switch dv.defaultParameters.pldaps.draw.photodiode.location
+if(p.trial.pldaps.draw.photodiode.use)
+    switch p.trial.pldaps.draw.photodiode.location
         case 1
-            rect = [dv.defaultParameters.display.winRect(1:2) dv.defaultParameters.display.winRect(1:2)+dv.defaultParameters.display.ppd];
+            rect = [p.trial.display.winRect(1:2) p.trial.display.winRect(1:2)+p.trial.display.ppd];
         case 2
-            rect = [dv.defaultParameters.display.winRect(1) dv.defaultParameters.display.winRect(4)-dv.defaultParameters.display.ppd ...
-                dv.defaultParameters.display.winRect(1)+dv.defaultParameters.display.ppd dv.defaultParameters.display.winRect(4)];
+            rect = [p.trial.display.winRect(1) p.trial.display.winRect(4)-p.trial.display.ppd ...
+                p.trial.display.winRect(1)+p.trial.display.ppd p.trial.display.winRect(4)];
         case 3
-            rect = [dv.defaultParameters.display.winRect(3)-dv.defaultParameters.display.ppd dv.defaultParameters.display.winRect(2)...
-                dv.defaultParameters.display.winRect(3) dv.defaultParameters.display.winRect(2)+dv.defaultParameters.display.ppd];
+            rect = [p.trial.display.winRect(3)-p.trial.display.ppd p.trial.display.winRect(2)...
+                p.trial.display.winRect(3) p.trial.display.winRect(2)+p.trial.display.ppd];
         case 4
-            rect = [dv.defaultParameters.display.winRect(3:4)-dv.defaultParameters.display.ppd dv.defaultParameters.display.winRect(3:4)];
+            rect = [p.trial.display.winRect(3:4)-p.trial.display.ppd p.trial.display.winRect(3:4)];
         otherwise
-            rect = [dv.defaultParameters.display.winRect(1) dv.defaultParameters.display.winRect(4)-dv.defaultParameters.display.ppd ...
-                dv.defaultParameters.display.winRect(1)+dv.defaultParameters.display.ppd dv.defaultParameters.display.winRect(4)];
+            rect = [p.trial.display.winRect(1) p.trial.display.winRect(4)-p.trial.display.ppd ...
+                p.trial.display.winRect(1)+p.trial.display.ppd p.trial.display.winRect(4)];
     end
     
-    dv.trial.pldaps.draw.photodiode.rect=rect;
+    p.trial.pldaps.draw.photodiode.rect=rect;
 end
