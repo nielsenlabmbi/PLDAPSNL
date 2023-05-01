b1ccc6c5b744795c7a9719002bd7b11a69562bba
Kristina <nielsenlabmbi@gmail.com>
2023-05-01 16:34:20 -0400

    patch rdk

---
+++ +pldapsExperiments/+dots/dots_FBAD5Settings_patch.m
@@ -0,0 +1,46 @@
+function s = dots_FBAD5Settings_patch
+
+%FEMALE
+%664901 = ANIMAL ID
+
+% % turn saving off
+% s.pldaps.nosave = 1;
+
+%set reward amounts
+s.behavior.reward.amount = [0.15 0.35 0.35 0.35];
+
+%set stimulus parameters
+s.stimulus.dotSize = 1.5;
+s.stimulus.dotDensity = 0.12; %dots/deg^2
+s.stimulus.dotColor = 0;
+s.stimulus.dotCoherence =  1; %regulated by staircase, this is start value
+s.stimulus.dotSpeed = 48; %deg/sec
+s.stimulus.dotLifetime = 25; %ms, 
+s.stimulus.direction = [0 180];
+s.stimulus.width=20; %deg
+s.stimulus.height=20; %deg
+s.stimulus.centerX=[300 1500]; %pixels
+s.stimulus.centerY=300;
+
+s.display.bgColor = [.5 .5 .5]; 
+
+%staircase parameters
+s.stimulus.stair=[0 1];
+s.stimulus.step=0.05;
+
+%viewing parameters
+s.display.viewdist = 75; 
+s.stimulus.durStim = 60; %sec
+s.stimulus.frameRate = 120;
+s.stimulus.duration.ITI = 1.5;
+s.stimulus.midpointIR = 1; %turn stimulus on when crossing midline
+
+
+
+% %turn adc channels off
+% s.datapixx.useAsPorts = 0;
+% s.datapixx.adc.channels = [];
+% 
+% %turn mouse input on
+% s.mouse.useAsPort = 1;
+% s.mouse.use = 1;
---
+++ +pldapsExperiments/+dots/dots_setup_free_patch.m
@@ -0,0 +1,45 @@
+function p = dots_setup_free_patch(p)
+%experiment setup file for a simple test experiment
+
+%% basic definitions
+p = pdsDefaultTrialStructureNL(p); 
+
+%% set the trial function: the function that gets called for each frame state
+p.trial.pldaps.trialFunction='pldapsExperiments.dots.dotstrial_free_patch';
+
+%% set general parameters
+p.trial.stimulus.forceCorrect = 1;
+
+%% set timing parameters
+p.trial.stimulus.duration.ITI = 3;
+
+%% conditions:
+side.par = 'direction';
+side.match=p.defaultParameters.stimulus.direction;
+
+%staircase on space bar
+for i = 1:length(p.defaultParameters.stimulus.stair)
+     cond(i).stair=p.defaultParameters.stimulus.stair(i);
+     cond(i).direction=p.defaultParameters.stimulus.direction;
+     cond(i).centerX=p.defaultParameters.stimulus.centerX;
+     c{i}=generateCondList(cond,side,'pseudo',250);
+end
+
+
+
+p.trial.allconditions = c;
+p.trialMem.whichConditions = 0;
+p.conditions=p.trial.allconditions{p.trialMem.whichConditions + 1};
+
+p.trial.pldaps.finish = length(p.conditions);
+
+
+
+%% display stats
+p.trialMem.stats.cond={'direction', 'stair'}; %conditions to display
+[A,B] = ndgrid(cond.direction,cond.stair);
+p.trialMem.stats.val = [A(:),B(:)]';
+nCond=size(p.trialMem.stats.val,2);
+p.trialMem.stats.count.correct=zeros(1,nCond);
+p.trialMem.stats.count.incorrect=zeros(1,nCond);
+p.trialMem.stats.count.Ntrial=zeros(1,nCond);
