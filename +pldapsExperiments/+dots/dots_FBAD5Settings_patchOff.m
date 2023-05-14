1470b2264f8e4110767f5ff99632380bdc841d5f
Kristina <nielsenlabmbi@gmail.com>
2023-05-14 17:08:12 -0400

    offset dot stimuli

---
+++ +pldapsExperiments/+dots/dots_FBAD5Settings_patchOff.m
@@ -0,0 +1,49 @@
+function s = dots_FBAD5Settings_patchOff
+
+%FEMALE
+%664901 = ANIMAL ID
+
+% % turn saving off
+% s.pldaps.nosave = 1;
+
+%set reward amounts
+s.behavior.reward.amount = [0.15 0.35 0.35 0.35];
+
+%set stimulus parameters
+s.stimulus.dotSize = 1.5;
+s.stimulus.dotDensity = 0.12; %dots/deg^2
+s.stimulus.dotColor = 0;
+s.stimulus.dotCoherence =  1; %regulated by staircase, this is start value
+s.stimulus.dotSpeed = 48; %deg/sec
+s.stimulus.dotLifetime = 25; %ms, 
+s.stimulus.direction = [0 180];
+s.stimulus.width=10; %deg
+s.stimulus.height=10; %deg
+s.stimulus.centerX=990; %pixels
+s.stimulus.centerY=540;
+s.stimulus.offset=[0 100 200];
+s.stimulus.stimSide=[-1 1];
+
+s.display.bgColor = [.5 .5 .5]; 
+
+%staircase parameters
+s.stimulus.stair=1; %[0 1] if on space bar;
+s.stimulus.step=0.05;
+
+%viewing parameters
+
+s.display.viewdist = 60; %cm
+s.stimulus.durStim = 0.3; %sec - can be further regulated with user keys
+s.stimulus.frameRate = 120;
+s.stimulus.duration.ITI = 0.2;
+s.stimulus.midpointIR = 1; %turn stimulus on when crossing midline
+
+
+
+% %turn adc channels off
+% s.datapixx.useAsPorts = 0;
+% s.datapixx.adc.channels = [];
+% 
+% %turn mouse input on
+% s.mouse.useAsPort = 1;
+% s.mouse.use = 1;
