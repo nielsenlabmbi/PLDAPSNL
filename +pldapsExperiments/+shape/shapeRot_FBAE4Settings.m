bd79e8020f81df000e3b67ab35572a7d008d34b4
Kristina <nielsenlabmbi@gmail.com>
2025-10-04 16:17:24 -0400

    shape rot

--- +pldapsExperiments/+shape/shapeRot_FBAE4Settings.m
+++ +pldapsExperiments/+shape/shapeRot_FBAE4Settings.m
@@ -9,9 +9,9 @@ s.behavior.reward.amount = [0.15 0.35 0.35 0.35];
 
 %set stimulus parameters
 s.stimulus.imgBase='/home/nielsenlab/shapeImg';
-s.stimulus.posImg='plus';
-s.stimulus.negImg='star';
-s.stimulus.filetype='jpeg';
+s.stimulus.posImg='star';
+s.stimulus.negImg='squareimg';
+s.stimulus.filetype='png';
 %s.stimulus.shapeScale=1; %scale factor for shape
 s.display.bgColor = [0 0 0];
 s.stimulus.rotShape=[0 1 2 3];%rotate none, positive, negative, or both shapes
