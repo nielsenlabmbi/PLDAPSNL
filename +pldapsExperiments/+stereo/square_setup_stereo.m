bf09c2f2fd815d0e72f9d259480ae1792905adfb
Kristina <nielsenlabmbi@gmail.com>
2025-10-22 14:34:51 -0400

    stereo color

---
+++ +pldapsExperiments/+stereo/square_setup_stereo.m
@@ -0,0 +1,32 @@
+function p=square_setup_stereo(p)
+
+%% basic definitions
+p = pdsDefaultTrialStructureNL(p); 
+
+%% set the trial function: the function that gets called for each frame state
+p.trial.pldaps.trialFunction='pldapsExperiments.stereo.squaretrial_dots_stereo';
+
+%% set general parameters
+p.trial.stimulus.forceCorrect = 1;
+
+%% set timing parameters
+p.trial.stimulus.duration.ITI = p.defaultParameters.stimulus.duration.ITI; %ITI in s
+
+
+%% conditions:
+c{1}=generateCondList_Gellerm(p.trial.stimulus.cond.Ncond);
+
+
+p.trial.allconditions = c;
+p.trialMem.whichConditions = 0; %index into trialslist; 0 is easier for mod
+p.conditions=p.trial.allconditions{1}; %set to first trials list
+
+p.trial.pldaps.finish = length(p.conditions);
+
+
+%% display stats
+%side counter
+pds.behavior.resetSideCounter(p);
+
+%condition counter
+pds.behavior.resetCondCounter(p,p.trial.stimulus.cond.counterNames,1);
---
+++ +pldapsExperiments/+stereo/square_stereo_XXXX0Settings.m
@@ -0,0 +1,33 @@
+function s = square_stereo_XXXX0Settings
+
+%set reward amounts
+s.behavior.reward.amount = [0.15 0.35 0.35 0.35];
+%set orientation and offsets
+s.stimulus.midpointIR = 0; %use midpoint IR beam 
+s.stimulus.angle = [0 1];
+
+
+%Full Size
+
+%Large but no longer touching screen edges
+s.stimulus.sizeX=500;
+s.stimulus.sizeY=175;
+
+s.stimulus.dotColor1=[1 0 0]; %red
+s.stimulus.dotColor2=[0 1 0]; %green
+s.stimulus.offStim=1.5;
+s.stimulus.dotSize = .3;% original 1.5
+s.stimulus.dotDensity = .005; %0.03; %dots/deg^2
+s.stimulus.dotColor1 = [1 0 0];
+s.stimulus.dotColor2 = [0 0 1];
+s.stimulus.centerX= 990; %pixels
+s.stimulus.centerY= 510;
+s.stimulus.duration.ITI = 1.5;
+
+%set up the viewing distance
+s.display.viewdist = 75; 
+s.display.useOverlay=0;
+
+s.stimulus.cond.Ncond=12; %stim side x color
+s.stimulus.cond.counterIdx=[1 1 1 1 3 3 2 2 2 2 4 4]; %index into the counter for each condition
+s.stimulus.cond.counterNames{1}={'L-Mono';'R-Mono';'L-Stereo';'R-Stereo'};
